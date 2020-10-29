3fb24377cebacebe0bf1fba0617367bc3cc3a3e4
superpuper777 <superfevrik@gmail.com>
2020-10-29 22:16:58 +0300

    Add indexes for improving speed

--- task/mongo-tasks.js
+++ task/mongo-tasks.js
@@ -24,6 +24,27 @@ async function before(db) {
     await db.collection('employees').ensureIndex({
         CustomerID: 1
     });
+    await db.collection('customers').createIndex({
+        CustomerID: 1
+    });
+    await db.collection('order-details').createIndex({
+        OrderID: 1
+    });
+    await db.collection('order-details').createIndex({
+        ProductID: 1
+    });
+    await db.collection('orders').createIndex({
+        OrderID: 1
+    });
+    await db.collection('orders').createIndex({
+        CustomerID: 1
+    });
+    await db.collection('orders').createIndex({
+        EmployeeID: 1
+    });
+    await db.collection('products').createIndex({
+        ProductID: 1
+    });
 }
 
 /**
